fe9dda3c01db7a96d82a43eeb855ecbcaf348365
christina511 <christina511ny@gmail.com>
2017-08-16 12:55:03 -0400

    Update Slide5.js

--- Slide5.js
+++ Slide5.js
@@ -1,7 +1,15 @@
 function setup() { 
   createCanvas(400, 400);
 	drawScene1();
+	//drawScene2();
 } 
+
+keyPressed = function() {
+	if (currentScene === 1){
+		drawScene2();
+	}
+};
+
 var currentScene;
 
 var drawScene1= function() { 
@@ -27,7 +35,32 @@ var drawScene1= function() {
 
 var continueButton=function(){
 	rect(115, 250, 175, 50);
-	textSize(25);
+	textSize(20);
 	fill(250, 250, 250);
-	text("Continue", 153, 285);
+	text("press SPACE to", 130, 270);
+	fill(250, 250, 250);
+	text("continue", 163, 290);
+}
+
+var drawScene2 = function() {
+	currentScene = 2;
+	//textbox
+	background(220);
+	strokeWeight(3);
+	fill(194, 255, 222);
+	rect(50, 50, 300, 300);
+	//text- 5d
+	textSize(32);
+	fill(0, 102, 153);
+	text("Instructions", 115, 100);
+	textSize(24);
+	fill(0, 102, 153);
+	text("The E meter shows how", 75, 150);
+	fill(0, 102, 153);
+	text("much you've eaten. Once", 65, 175);
+	fill(0, 102, 153);
+	text("it's filled, you cannot", 90, 200);
+	fill(0, 102, 153);
+	text("eat anymore.", 125, 225);
+	continueButton();
 }
