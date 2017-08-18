5b75571c6e854bb85294f23c1a34e68999f1baa4
JustALittleBit <31043555+JustALitteBit@users.noreply.github.com>
2017-08-18 13:56:50 -0400

    Add files via upload

---
+++ GuiltTrip4.js
@@ -0,0 +1,10 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var GT4 = document.images["GT4"];
+
+
+function Slide3(){
+  ctx.drawImage(GT4,400,75);
+  // document.getElementById('GT2').setAttribute("style","visibility:hidden");
+  // document.getElementById('GT').setAttribute("style","visibility:hidden");
+}
