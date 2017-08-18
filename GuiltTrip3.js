f451910026be6de7c05e0da5a8f8ac88f6643865
JustALittleBit <31043555+JustALitteBit@users.noreply.github.com>
2017-08-18 13:24:53 -0400

    Add files via upload

---
+++ GuiltTrip3.js
@@ -0,0 +1,10 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var GT3 = document.images["GT3"];
+
+
+Slide3(){
+  ctx.drawImage(GT3,400,75);
+  document.getElementById('GT2').setAttribute("style","visibility:hidden");
+  document.getElementById('GT').setAttribute("style","visibility:hidden");
+}
