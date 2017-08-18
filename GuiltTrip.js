0d5b793aa1ea2cf0ea2f74e78ce50cb78f210734
JustALittleBit <31043555+JustALitteBit@users.noreply.github.com>
2017-08-18 13:34:06 -0400

    Update GuiltTrip.js

--- GuiltTrip.js
+++ GuiltTrip.js
@@ -3,7 +3,7 @@ var ctx = c.getContext("2d");
 var GT = document.images["GT"];
 
 
-Slide1(){
+function Slide1(){
   ctx.drawImage(GT,400,75);
   document.getElementById('GT2').setAttribute("style","visibility:hidden");
 }
